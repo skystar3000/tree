ad904bcf0c74c9e84019a42fc434b50686639dfc
skystar3000 <skystar3000@163.com>
2018-07-10 19:06:13 +0800

    Revert "heatControl"
    This reverts commit 9ea16db8ecc9afeb6c59a4ff79a35c7f549b434c.

--- gitfile/t_heatControl.h
+++
@@ -1,8 +0,0 @@
-#ifndef _t_heatControl_function_
-#define _t_heatControl_function_
-
-extern short nowHeat;
-extern short allowShootNum;
-extern short heatControl(short addingHeat, short coolingHeat, short limitHeat, short judgeHeat, short coolingWaitTime);
-
-#endif
